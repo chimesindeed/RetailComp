5cf3a18b51fd7faed9b7f14cf8ce5d6c698cefcf
dimitri.gedda <gratitudeadore@fastmail.com>
2020-11-26 14:35:23 -0500

    added Header component

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+
+import React from 'react'
+import { styles } from  './stylesheet/stylesheet'
+import { css } from 'aphrodite'
+
+const Header = () => {
+	return (
+		<div className={css(styles.background)}>
+			<div>
+				<div className={css(styles.text)}>Salvation Army</div>
+				<div className={css(styles.text2)}></div>
+			</div>
+			
+			<div>
+
+			</div>
+		</div>
+		)
+}
+export default Header
