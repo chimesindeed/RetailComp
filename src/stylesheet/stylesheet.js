1d82a6a0d106be6a824678532be9b198b8289a76
dimitri.gedda <gratitudeadore@fastmail.com>
2020-11-26 14:34:27 -0500

    created stylesheet | used aphrodite library to give Stylesheet.create functionality

---
+++ src/stylesheet/stylesheet.js
@@ -0,0 +1,132 @@
+import { StyleSheet } from 'aphrodite'
+
+export const styles = StyleSheet.create({
+
+	background: {
+		paddingTop: 30,
+		padding: 2,
+		paddingTop: 15,
+		backgroundColor: '#cacecb',
+		borderWidth: 4,
+		borderColor: '#6f7170',
+	},
+	body: {
+		backgroundColor: 'beige'
+	},
+	button: {
+		width:30
+	},
+	date: {
+		flexDirection: 'row',
+		fontSize: 18,
+		paddingLeft: 25,
+		textAlign: 'left',
+		color: 'yellow',
+		backgroundColor: '#c5c9c6',
+		paddingBottom: 15,
+		lineHeight: 25,
+		letterSpacing: 5,
+	},
+	discountFiller: {
+		backgroundColor: '#004e7c',
+		padding: 3
+	},
+	discountBackground: {
+		backgroundColor: '#004e7c',
+		paddingBottom: 15
+	},
+	discountHeader: {
+		color: 'white',
+		fontWeight: '600',
+		fontSize: 25,
+		lineHeight: 27,
+		letterSpacing: 2,
+		paddingBottom: 15,
+		textAlign: 'left'
+	},
+	highlightBlue: {
+	color: 'grey',
+	fontWeight: '700',
+	fontSize: 15,
+	lineHeight: 29,
+	letterSpacing:3 ,
+	borderWidth: 1,
+	borderColor: 'grey',
+	paddingLeft: 4,
+	},
+	highlightGreen: {
+	color: 'green',
+	fontWeight: '700',
+	fontSize: 15,
+	lineHeight: 29,
+	letterSpacing:3 ,
+	borderWidth: 1,
+	borderColor: 'green',
+	paddingLeft: 4,
+	},
+	highlightYellow: {
+	color: 'yellow',
+	fontWeight: '700',
+	fontSize: 15,
+	lineHeight: 29,
+	letterSpacing:3 ,
+	borderWidth: 1,
+	borderColor: 'yellow',
+	paddingLeft: 4,
+	},
+	highlightPink: {
+	color: 'pink',
+	fontWeight: '700',
+	fontSize: 15,
+	lineHeight: 29,
+	letterSpacing:3 ,
+	borderWidth: 1,
+	borderColor: 'pink',
+	paddingLeft: 4,
+	},
+	highlightWhite: {
+	color: 'white',
+	fontWeight: '700',
+	fontSize: 15,
+	lineHeight: 29,
+	letterSpacing:3 ,
+	borderWidth: 1,
+	borderColor: 'white',
+	paddingLeft: 4,
+	},
+	incomingText: {
+		color: 'red',
+		fontSize: 9
+	},
+	sectionContainer: {
+		marginTop: 32,
+		paddingHorizontal: 24
+	},
+	sectionDescription: {
+		marginTop: 8,
+		fontSize: 18,
+		fontWeight: '400',
+		color: 'grey'
+	},
+	sectionTitle: {
+		fontSize: 24,
+		fontWeight: '600',
+		color: 'black',
+	},
+	text: {
+		fontSize: 33,
+		fontWeight: '600',
+		textAlign: 'left',
+		color: '#69bedd',
+		letterSpacing: 1,
+		textAlign: 'center'
+	},
+	text2: {
+		fontSize: 10,
+		fontWeight: '600',
+		textAlign: 'left',
+		color: 'green',
+		letterSpacing: 1,
+		textAlign: 'center'
+	},
+});
