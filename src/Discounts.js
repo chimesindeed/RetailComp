eaac92dc8dcf989bdda0a2352be3eaf7d628b828
dimitri.gedda <gratitudeadore@fastmail.com>
2020-11-26 14:44:38 -0500

    Added  empty Discounts component with this.props.children to nest various color components depending on week

---
+++ src/Discounts.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+class Discounts extends React.Component{
+	constructor(props){
+		super(props)
+	}
+	render(){return this.props.children}
+}
+
+export default Discounts
