991064eb8d98a170d4170618c098f41b017fa176
redux-dev <reduxdev@localhost.localdomain>
2020-11-25 23:46:19 -0500

    initial commit.  setup redux store with increment function to make sure redux working correctly

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import { connect } from 'react-redux'
+import { INCREMENT} from './actions'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  handleClick = () =>{
+    return (this.props.INCREMENT())
+  }
+  render() {
+    return (
+      <div>
+        <h1>App</h1>
+
+        {this.props.week}
+
+        <button onClick={this.handleClick}>increment</button>
+      </div>
+    );
+  }
+
 }
-
-export default App;
+const mapStateToProps = (state) => {
+  return ({week: state.week})
+}
+export default connect(mapStateToProps, {INCREMENT})(App)
