17cb53244381d9a268b99d31c8bb5a069d2c77e1
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-05 20:03:15 -0500

    added index action for stores and notes controllers

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,2 +1,11 @@
 class StoresController < ApplicationController
+	def index
+		stores = Store.all
+		render json: stores
+	end
+
+	def show
+		store = Store.findbyid(:id)
+		render json: store
+	end
 end
