48ef1856ccd864975aec575ce62cbb65fc848e27
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-07 01:05:24 -0500

    added 'asyncFetchStores' actionCreator with inner Thunk

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,3 +1,4 @@
+import * as ADAPTER from '../adapter.js'
 
 export const INCREMENT = () => {
 	return ({type: "INCREMENT"})
@@ -7,6 +8,12 @@ export const TO_ONE = () => {
 	return ({type: "TO_ONE"})
 }
 
-export const GET_STORES = () => {
-	return ({type: "GET_STORES"})
+export const GET_STORES = (data) => {return ({type: "GET_STORES", payload: data}) }
+
+export const asyncFetchStores = () => {
+	return function(dispatch){
+		return ADAPTER.getStores().then(
+			(response) => dispatch(GET_STORES(response))
+		)
+	}
 }
