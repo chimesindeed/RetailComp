0d76a1ff1b48baa664ec7513589808e69d48d94c
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-07 01:14:56 -0500

    applied thunk middleware to reducer in createStore function

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux'
 import {rootReducer} from './reducers'
 import { applyMiddleware } from 'redux'
-import { thunk } from 'redux-thunk'
+import  thunk from 'redux-thunk'
 
 export const reduxStore = createStore(rootReducer, applyMiddleware(thunk))
