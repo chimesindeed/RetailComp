da3e11263a0db2109d70a9882aadf12728042fd7
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-06 19:28:34 -0500

    added Redux Thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,6 @@
 import { createStore } from 'redux'
 import {rootReducer} from './reducers'
+import { applyMiddleware } from 'redux'
+import { thunk } from 'redux-thunk'
 
-export const reduxStore = createStore(rootReducer)
+export const reduxStore = createStore(rootReducer, applyMiddleware(thunk))
