a8b7fe1b10aee3edd762a8d0f5a87c874914a520
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-07 01:13:03 -0500

    added 'stores: key to reducer which will receive payload of entire store list from api

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,10 +1,11 @@
 
-const initialState = {week: 5}
+const initialState = {week: 5, stores: []}
 
 export const rootReducer = (state = initialState, action)=> {
 	switch(action.type){
 		case "INCREMENT": return (Object.assign({}, state, {week: state.week + 1}))
 		case "TO_ONE": return (Object.assign({}, state, {week: state.week - 4}))
+		case "GET_STORES": return (Object.assign({}, state, {stores: action.payload}))
 		default: return state
 	}
 	
