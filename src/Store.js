a993fa2e9d268047987e9828987b935495aa2b5b
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-08 08:24:48 -0500

    consume properties assigned in StoresContainer in Store Component

--- src/Store.js
+++ src/Store.js
@@ -3,8 +3,14 @@ import React from 'react'
 const Store = (props) => {
 	return (
 		<div>
-			<h2>{props.title}</h2>
 			<h5>{props.id}</h5>
+			<h5>{props.number}</h5>
+			<h5>{props.name}</h5>
+			<h5>{props.address}</h5>
+			<h5>{props.city}</h5>
+			<h5>{props.state}</h5>
+			<h5>{props.zip}</h5>
+
 		</div>
 		
 
