02004a52c2fbf27547258bf560709db86ac81a7c
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-08 15:10:09 -0500

    added styling for Store List feature in 'stylesheet-stores.js'

--- src/Store.js
+++ src/Store.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import { css } from 'aphrodite'
+import { styles } from  './stylesheet/stylesheet-stores.js'
 
 const Store = (props) => {
 	return (
-		<div>
-			<h5>{props.id}</h5>
-			<h5>{props.number}</h5>
-			<h5>{props.name}</h5>
-			<h5>{props.address}</h5>
-			<h5>{props.city}</h5>
-			<h5>{props.state}</h5>
-			<h5>{props.zip}</h5>
+		<div className= {(css(styles.storeBackground))}>
+			<h3>Store Name: {props.name}</h3>
+			<h5>Store Number: {props.number}</h5>
+			<h5>Address: {props.address}</h5>
+			<h5>City: {props.city}</h5>
+			<h5>State: {props.state}</h5>
+			<h5>Zip: {props.zip}</h5>
 
 		</div>
 		
--- src/stylesheet/stylesheet.js
+++ src/stylesheet/stylesheet.js
@@ -1,19 +1,9 @@
-import { StyleSheet } from 'aphrodite'
-
 export const styles = StyleSheet.create({
 
-	background: {
-	
-		paddingTop: 2,
-		paddingBottom: 8,
-		backgroundColor: 'maroon',
-		borderWidth: 4,
-		borderColor: '#6f7170',
-		lineHeight: 2.3,
-	},
-	body: {
+body: {
 		backgroundColor: 'beige'
 	},
+
 	button: {
 		width:30
 	},
@@ -28,77 +18,7 @@ export const styles = StyleSheet.create({
 		lineHeight: 25,
 		letterSpacing: 5,
 	},
-	discountFiller: {
-		backgroundColor: 'teal',
-		padding: 1
-	},
-	discountBackground: {
-		backgroundColor: 'black',
-		paddingBottom: 15
-	},
-	discountHeader: {
-		color: 'white',
-		fontWeight: '600',
-		fontSize: 30,
-		lineHeight: 1.8,
-		letterSpacing: 2,
-		paddingBottom: 15,
-		textAlign: 'left'
-	},
-	highlightBlue: {
-	color: 'blue',
-	fontWeight: '700',
-	fontSize: 20,
-	lineHeight: 3,
-	letterSpacing:3 ,
-	borderWidth: 1,
-	borderColor: 'grey',
-	paddingLeft: 4,
-	},
-	highlightGreen: {
-	color: 'green',
-	fontWeight: '700',
-	fontSize: 20,
-	lineHeight: 3,
-	letterSpacing:3 ,
-	borderWidth: 1,
-	borderColor: 'green',
-	paddingLeft: 4,
-	},
-	highlightYellow: {
-	color: 'yellow',
-	fontWeight: '700',
-	fontSize: 20,
-	lineHeight: 3,
-	letterSpacing:3 ,
-	borderWidth: 1,
-	borderColor: 'yellow',
-	paddingLeft: 4,
-	},
-	highlightPink: {
-	color: 'pink',
-	fontWeight: '700',
-	fontSize: 20,
-	lineHeight: 3,
-	letterSpacing:3 ,
-	borderWidth: 1,
-	borderColor: 'pink',
-	paddingLeft: 4,
-	},
-	highlightWhite: {
-	color: 'white',
-	fontWeight: '700',
-	fontSize: 20,
-	lineHeight: 3,
-	letterSpacing:3 ,
-	borderWidth: 1,
-	borderColor: 'white',
-	paddingLeft: 4,
-	},
-	incomingText: {
-		color: 'red',
-		fontSize: 14
-	},
+	
 	sectionContainer: {
 		marginTop: 32,
 		paddingHorizontal: 24
@@ -114,18 +34,4 @@ export const styles = StyleSheet.create({
 		fontWeight: '600',
 		color: 'black',
 	},
-	text: {
-		fontSize: 60,
-		fontWeight: '600',
-		color: 'skyblue',
-		letterSpacing: 1,
-		textAlign: 'center'
-	},
-	text2: {
-		fontSize: 10,
-		fontWeight: '600',
-		color: 'green',
-		letterSpacing: 1,
-		textAlign: 'center'
-	},
-});
+	})
