37e7d8b31302abfbc827367380288fd598880770
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-08 08:06:16 -0500

    stubbed out C.R.U.D. actions in Stores Controller

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,11 +1,24 @@
 class StoresController < ApplicationController
 	def index
 		stores = Store.all
+
 		render json: stores
 	end
 
-	def show
-		store = Store.findbyid(:id)
-		render json: store
+	def create
+		store = Store.create(store_params)
+
+		render json :store
 	end
+
+	def update
+		store = Store.find(params[:id])
+
+		render json :store
+	end
+
+	def note_params
+		params.require(:store).permit(:number, :name, :address, :city, :state, :zip)
+	end
+
 end
