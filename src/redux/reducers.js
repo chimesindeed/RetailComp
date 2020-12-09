4986c935621d7c66b56385b462722c7206ed56f7
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-09 04:59:36 -0500

    added 'store' object to Redux state in reducers

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,12 +1,13 @@
 
 
-const initialState = {week: 5, stores: []}
+const initialState = {week: 5, stores: [], store: []}
 
 export const rootReducer = (state = initialState, action)=> {
 	switch(action.type){
 		case "INCREMENT": return (Object.assign({}, state, {week: state.week + 1}))
 		case "TO_ONE": return (Object.assign({}, state, {week: state.week - 4}))
 		case "GET_STORES": return (Object.assign({}, state, {stores: action.payload}))
+		case "GET_STORE": return (Object.assign({}, state, {store: action.payload}))
 		default: return state
 	}
 	
