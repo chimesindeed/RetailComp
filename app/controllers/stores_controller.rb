ed72e4ae12ba8fe9ef2e5937310da1ca4598b292
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-12 18:26:41 -0500

    renamed delete method to destroy in Stores Controller. Delete method now working

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -5,19 +5,28 @@ class StoresController < ApplicationController
 		render json: stores
 	end
 
+	def show
+		store = Store.find(params[:id])
+
+		render json: store
+	end
+
 	def create
 		store = Store.create(store_params)
 
-		render json :store
+		render json: store
 	end
 
 	def update
 		store = Store.find(params[:id])
 
-		render json :store
+		render json: store
 	end
 
-	def note_params
+	def destroy
+		store = Store.find(params[:id]).destroy
+	end
+	def store_params
 		params.require(:store).permit(:number, :name, :address, :city, :state, :zip)
 	end
 
