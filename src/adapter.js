2ce87249d78654cbad866521067f2cd6326d81be
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-12 18:36:50 -0500

    adapter.js | Stored urls for 'Store' and 'Stores' in consts.  Added fetch request method 'delete'

--- src/adapter.js
+++ src/adapter.js
@@ -1,17 +1,29 @@
-
-
+const storesURL = 'http://localhost:3001/stores'
+const storeURL  = (id = null) =>{return `http://localhost:3001/stores/${id}`}
+//______________________________________________________________________________
 export async function getStores(){
 	let stores;
 	let storesAsJSON;
-	stores = await fetch('http://localhost:3001/stores')
+	stores = await fetch(storesURL)
 	storesAsJSON = await stores.json()
 	return storesAsJSON
 }
-
+//______________________________________________________________________________
 export async function getStore(id){
 	let store;
 	let storeAsJSON;
-	store = await fetch(`http://localhost:3001/stores/${id}`)
+	store = await fetch(storeURL(id))
 	storeAsJSON = await store.json()
 	return storeAsJSON
 }
+//______________________________________________________________________________
+export async function deleteStore(id){
+	let store;
+	store = await fetch(storeURL(id),{
+		method: 'delete',
+		headers: {
+			'Content-type': 'application/json'
+		}
+	})
+}
+//______________________________________________________________________________
