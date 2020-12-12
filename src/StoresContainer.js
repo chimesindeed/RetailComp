7dee447dc0b0e10d4d7b23861173566e015e1a18
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-12 18:43:02 -0500

    when single store selected delete button deletes store

--- src/StoresContainer.js
+++ src/StoresContainer.js
@@ -4,13 +4,14 @@ import { Link } from 'react-router-dom'
 import { css } from 'aphrodite'
 import { styles } from  './stylesheet/stylesheet-stores.js'
 import Store from './Store'
-import { asyncFetchStores, asyncFetchStore } from './redux/actions'
+import { asyncFetchStores, asyncFetchStore} from './redux/actions'
+import { asyncDeleteStore } from './adapter.js'
 
 class StoresContainer extends React.Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			flipped: "allStores"
+			toggle: "allStores"
 		}
 	}
 	componentDidMount = () => {
@@ -18,43 +19,42 @@ class StoresContainer extends React.Component {
 	}
 	selectStore = value => () => {
 		this.props.asyncFetchStore(value);
-		this.setState({flipped: "clickedStore"})
+		this.setState({toggle: "clickedStore"})
 	}
 	
 	handleBackPressed = () => {
-		this.setState({flipped: "allStores"})
+		this.setState({toggle: "allStores"})
 	}
-		
-	
-	render = () => {
-		return (
-			<div>
-				<div className={css(styles.discountsLink)}><Link to={"/discounts"}>DISCOUNTS</Link></div>
-				{
-					this.state.flipped==="allStores"
-						?
-							(
-					this.props.allStores.map((store)=>{
-						return(
-							<div key={store.id} onClick={this.selectStore(store.id)}>
-								<Store
-									id={store.id}
-									number={store.number}
-									name={store.name}
-									address={store.address}
-									city={store.city}
-									state={store.state}
-									zip={store.zip}
-								/>
-							</div>
-						)	
-					})
-							)
+	handleDeletePressed = () => {
+		asyncDeleteStore(this.props.selectedStore.id)
+		this.setState({toggle: "allStores"})
+	}
 
-						:
+		renderStores = () => {
+			return(
+				this.state.toggle==="allStores"
+					?
+						(
+							this.props.allStores.map((store)=>{
+								return(
+									<div key={store.id} onClick={this.selectStore(store.id)}>
+										<Store
+											id={store.id}
+											number={store.number}
+											name={store.name}
+											address={store.address}
+											city={store.city}
+											state={store.state}
+											zip={store.zip}
+										/>
+									</div>
+								)	
+							})
+						)
 
-							(
-					
+					:
+
+						(
 							<div>
 								<Store
 									id={this.props.selectedStore.id}
@@ -65,14 +65,28 @@ class StoresContainer extends React.Component {
 									state={this.props.selectedStore.state}
 									zip={this.props.selectedStore.zip}
 								/>
-								<div><button onClick={this.handleBackPressed}>back</button></div>
+								<div>
+									<button onClick={this.handleBackPressed}>back</button>
+									<button onClick={this.handleDeletePressed}>delete</button>
+									<button onClick={this.handleBackPressed}>update</button>
+								</div>
 							</div>
 
 						)
-					}
+			)
+
+		}
+
+	
+	render = () => {
+		return (
+			<div>
+				<div className={css(styles.discountsLink)}>
+					<Link to={"/discounts"}>DISCOUNTS</Link>
+				</div>
+				{this.renderStores()}
 			</div>
 		)
-
 	}
 
 }
