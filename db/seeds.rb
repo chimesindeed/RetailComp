69896844206979894ed6f6daa6be358ee44fae22
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-12 20:22:41 -0500

    seeded database with 2 Stores

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,21 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+	stores = Store.create([
+		{ number: 5,
+		  name: 'Store1',
+		  address: 'address1',
+		  city: 'city1',
+		  state: 'state1',
+		  zip: 'zip1'
+		},
+
+		{ number: 3,
+                  name: 'Store2',
+                  address: 'address2',
+                  city: 'city2',
+                  state: 'state2',
+                  zip: 'zip2'
+                }
+ 	])
 #   Character.create(name: 'Luke', movie: movies.first)
