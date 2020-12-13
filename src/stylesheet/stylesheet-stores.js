0d4efcedb25d9db20ecbd62783f772103a776c97
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-13 12:23:39 -0500

    updated styling for Stores and Store Components in stylesheet-stores.js.

--- src/stylesheet/stylesheet-stores.js
+++ src/stylesheet/stylesheet-stores.js
@@ -2,16 +2,25 @@ import { StyleSheet } from 'aphrodite'
 
 export const styles = StyleSheet.create({
 	
-	storeBackground: {
-		backgroundColor: 'skyblue',
-		textAlign: 'left',
-		color: 'black',
-	},
-
 	discountsLink: {
 		textAlign: 'right',
 		backgroundColor: 'maroon',
 		color: 'white'
 
 	},
+
+	storesBackground: {
+		backgroundColor: 'blue',
+		color: 'white',
+		paddingLeft: 20
+	},
+
+	storeBackground: {
+		textAlign: 'center'
+	},
+
+	optionsDiv: {
+		display: 'flex',
+		justifyContent: 'center'
+	}
 });
