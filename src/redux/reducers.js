d7847393b5d151363ec3d6b6c0c55b321a39e1f8
dimitri.gedda <gratitudeadore@fastmail.com>
2020-12-17 06:48:55 -0500

    added 'toggle' key to Redux store.  Responds to SELECTED_STORE, ALL_STORES, and UPDATE_STORE Action Creators.  This is to replace toggle in local state in StoresContainer Component

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,13 +1,19 @@
 
 
-const initialState = {week: 5, stores: [], store: []}
+const initialState = {week: 5, stores: [], store: [], toggle: 'allStores'}
 
 export const rootReducer = (state = initialState, action)=> {
 	switch(action.type){
 		case "INCREMENT": return (Object.assign({}, state, {week: state.week + 1}))
 		case "TO_ONE": return (Object.assign({}, state, {week: state.week - 4}))
+		
 		case "GET_STORES": return (Object.assign({}, state, {stores: action.payload}))
 		case "GET_STORE": return (Object.assign({}, state, {store: action.payload}))
+
+		case "SELECTED_STORE": return (Object.assign({}, state, {toggle: 'clickedStore'}))
+		case "ALL_STORES": return (Object.assign({}, state, {toggle: 'allStores'}))
+		case "UPDATE_STORE": return (Object.assign({}, state, {toggle: 'updateStore'}))
+
 		default: return state
 	}
 	
