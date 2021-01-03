3ff11605567f4b53711d5abaaa607870ec06ae4e
dimitri.gedda <gratitudeadore@fastmail.com>
2021-01-03 17:34:30 -0500

    in UpdateStoreForm changed initial local state values from null to empty string

--- src/UpdateStoreForm.js
+++ src/UpdateStoreForm.js
@@ -7,23 +7,22 @@ class UpdateStoreForm extends React.Component{
 	constructor(props){
 		super(props)
 		this.state = {
-			name: null,
-			address: null,
-			city: null,
-			state: null,
-			zip: null,
-			editedYet: false
+			name: "",
+			address: "",
+			city: "",
+			state: "",
+			zip: ""
 		}
 	}
 	
 	handleSubmit = (e) => {
 		e.preventDefault()
 		let name; let address; let city; let state; let zip;
-		this.state.name===null ? name = this.props.name : name = this.state.name
-		this.state.address===null ? address = this.props.address : address = this.state.address
-		this.state.city===null ? city = this.props.city : city = this.state.city
-		this.state.state===null ? state = this.props.state: state= this.state.state
-		this.state.zip===null ? zip = this.props.zip : zip = this.state.zip
+		this.state.name==="" ? name = this.props.name : name = this.state.name
+		this.state.address==="" ? address = this.props.address : address = this.state.address
+		this.state.city==="" ? city = this.props.city : city = this.state.city
+		this.state.state==="" ? state = this.props.state: state= this.state.state
+		this.state.zip==="" ? zip = this.props.zip : zip = this.state.zip
 		return(
 			ADAPTER.updateStore(
 				this.props.id,
@@ -45,7 +44,7 @@ class UpdateStoreForm extends React.Component{
 		
 			<div className= {this.props.className}>
 				<form onSubmit={this.handleSubmit} style={{justifyContent: 'left'}}>
-<h2><label>Store Name <input onChange={(e)=>{this.setState({name: e.target.value})}} placeholder={this.props.name} value={this.state.name}/></label></h2>
+<h2><label>Store Name <input onChange={(e)=>{this.setState({name: e.target.value})}} placeholder={this.props.name} value={this.state.name != null ? this.state.name.toLowerCase() : this.state.name}/></label></h2>
 <h3><label>Store Number <input placeholder={this.props.number} disabled/></label></h3> 
 <h3><label>Store Address <input onChange={(e)=>{this.setState({address: e.target.value})}}placeholder={this.props.address}value={this.state.address}/></label></h3>
 <h3><label>Store City <input onChange={(e)=>{this.setState({city: e.target.value})}}placeholder={this.props.city}value={this.state.city}/></label></h3>
