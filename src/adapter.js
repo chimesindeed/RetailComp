b930985837c790fe1d298516db74dac9822d3973
dimitri.gedda <gratitudeadore@fastmail.com>
2021-01-03 17:15:34 -0500

    cleaned up fetch requests. Added noteURL and delete note method

--- src/adapter.js
+++ src/adapter.js
@@ -1,5 +1,7 @@
 const storesURL = 'http://localhost:3001/stores'
-const storeURL  = (id = null) =>{return `http://localhost:3001/stores/${id}`}
+const storeURL  = (id = null) => { return `http://localhost:3001/stores/${id}` }
+const notesURL = 'http://localhost:3001/notes'
+const noteURL = (id = null) => { return `http://localhost:3001/notes/${id}` }
 //______________________________________________________________________________
 export async function getStores(){
 	let stores;
@@ -26,6 +28,15 @@ export async function deleteStore(id){
 	})
 }
 //______________________________________________________________________________
+export async function deleteNote(id){
+	await fetch(noteURL(id),{
+		method: 'delete',
+		headers: {
+			'Content-type': 'application/json'
+		}
+	})
+}
+//______________________________________________________________________________
 export async function updateStore(id, name, address, city, state, zip){
 	const store = {
 		id: id,
@@ -39,14 +50,17 @@ export async function updateStore(id, name, address, city, state, zip){
 	let updatedStoreAsJSON;
 	updatedStore = await fetch(storeURL(id),{
 		method: 'put',
+		
 		headers: {
 			'Content-type': 'application/json'
 		},
 
 		body: JSON.stringify({store})
     
-    })
-   	updatedStoreAsJSON = await updatedStore.json()
+  })
+
+  updatedStoreAsJSON = await updatedStore.json()
+	
 	return updatedStoreAsJSON
     	
 	}
@@ -72,9 +86,31 @@ export async function createStore(name, number, address, city, state, zip){
 		body: JSON.stringify({store})
     
     })
+   	
    	createdStoreAsJSON = await createdStore.json()
-	return createdStoreAsJSON
-    	
+		
+		return createdStoreAsJSON    	
 	}
 
 //______________________________________________________________________________
+export async function createNote(note_body, store_id){
+	const note = {
+		note_body: note_body,
+		store_id: store_id
+	}
+	let createdNote;
+	let createdNoteAsJSON;
+	createdNote = await fetch(notesURL,{
+		method: 'post',
+		headers: {
+			'Content-type': 'application/json'
+		},
+		
+		body: JSON.stringify({note})
+
+	})
+
+	createdNoteAsJSON = await createdNote.json()
+
+	return createdNoteAsJSON
+}
